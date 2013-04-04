f788926a6034bba50f801e2eebb8b1be33183588
Hugh Kennedy <hughskennedy@gmail.com>
2013-04-04 23:46:38 +1100

    IE

--- index.js
+++ index.js
@@ -7,18 +7,29 @@ var BLANK = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAO
 
 function loaded(image, callback) {
   var src
+    , old
     , onload
 
   if (!image.nodeName) return callback(new Error('First argument must be an image element'))
   if (image.nodeName.toLowerCase() !== 'img') return callback(new Error('Element supplied is not an image'))
   if (image.src  && image.complete && image.naturalWidth !== undefined) return callback(null, true)
 
+  old = !image.addEventListener
+
   function loaded() {
-    image.removeEventListener('load', loaded, false)
+    if (old) {
+      image.detachEvent('onload', loaded)
+    } else {
+      image.removeEventListener('load', loaded, false)
+    }
     callback(null, false)
   }
 
-  image.addEventListener('load', loaded, false)
+  if (old) {
+    image.attachEvent('onload', loaded)
+  } else {
+    image.addEventListener('load', loaded, false)
+  }
 
   if (image.readyState || image.complete) {
     src = image.src
